ec6cfb511df75821f982df4a2a50ae5a9c705b06
Mohamed00077 <mohamed.diabagate@epitech.eu>
2026-08-11 01:29:22 +0000

    Ajout des animations et rotation du personnage vers la direction de deplacement

--- mon-jeu-action-aventure/player.gd
+++ mon-jeu-action-aventure/player.gd
@@ -1,17 +1,18 @@
 extends CharacterBody3D
-const SPEED = 7.0
-const JUMP_VELOCITY = 4.5
+const SPEED = 5.0
+const JUMP_VELOCITY = 7.0
+const ROTATION_SPEED = 10.0
+
+@onready var anim_player: AnimationPlayer = $Barbarian/AnimationPlayer
+@onready var barbarian: Node3D = $Barbarian
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get input from keyboard AND from the virtual joystick.
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	
 	var joysticks = get_tree().get_nodes_in_group("joystick")
@@ -24,8 +25,18 @@ func _physics_process(delta: float) -> void:
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
+		
+		var target_angle = atan2(direction.x, direction.z)
+		barbarian.rotation.y = lerp_angle(barbarian.rotation.y, target_angle, delta * ROTATION_SPEED)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+
+	if not is_on_floor():
+		anim_player.play("Jump_Idle")
+	elif direction.length() > 0.1:
+		anim_player.play("Running_B")
+	else:
+		anim_player.play("Idle")
